fe4128ce4e57b805d94bdb5e323d594b7b4d53c2
RenatoFBabi <renatofrancosobabichak@gmail.com>
2024-10-11 11:42:22 -0300

    Mais botões adicionados

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -17,6 +17,14 @@ const Home = ()=>{
                <span className="border border-blue-500 rounded-2xl px-2 my-1 mx-1">#CSS </span>
                <span className="border border-blue-500 rounded-2xl px-2 my-1 mx-1">#JAVASCRIPT </span>
             </div>
+
+<div className="flex flex-wrap items-center rounded-b-xl border-t-2 bg-white">
+    <button className="border rounded-2xl bg-blue-600 text-white shadow-sm p-1 px-2 m-2">
+        Projetos
+    </button>
+    <button className="border-2 border-blue-600 rounded-2xl text-blue shadow-sm p-1 px-2 m-2"></button>
+</div>
+
         </div>
 
         </>
